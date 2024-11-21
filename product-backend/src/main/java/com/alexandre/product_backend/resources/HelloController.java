7d0b231ef14a2a9eb2ee3ddc07477d208f5dff65
Alexandre <alexandrevelasques@hotmail.com>
2024-11-21 02:29:22 -0300

    first commit

---
+++ product-backend/src/main/java/com/alexandre/product_backend/resources/HelloController.java
@@ -0,0 +1,14 @@
+package com.alexandre.product_backend.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("hello") 
+    public String getHello() {
+        return "Hello spring boot";
+    }
+
+}
